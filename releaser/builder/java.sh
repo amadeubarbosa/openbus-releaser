d49591e0a7ed9b6097e3683b1340eafca756ce77
Amadeu Andrade Barbosa Junior <amadeu@tecgraf.puc-rio.br>
2016-09-19 17:05:36 -0300

    [OPENBUS-2976] ajuste no java.sh para suportar alteração na plataforma

--- releaser/builder/java.sh
+++ releaser/builder/java.sh
@@ -2,19 +2,22 @@
 
 source releaser/builder/utils.sh
 
-assert_arg "$1" "java.sh <version>"
+assert_arg "$1" "java.sh <version> <jdk label>"
 
 version=$1
+platform=$2
 name=java
 modules="\
 openbus-java-$version \
 licenses-2013 \
 "
-jre="jre6"
+if [ "$platform" == "" ]; then
+  platform="jre6"
+fi
 
 # define a custom profile for 'makepack' to consume
 assert_dir $OPENBUS_BUILD
 echo "openbus-java-$version +dependencies +dev
 licenses-2013" > $OPENBUS_BUILD/$name
 
-makepack $name $version "$modules" $jre
+makepack $name $version "$modules" $platform
